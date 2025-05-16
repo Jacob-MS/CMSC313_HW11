1cde5cd75054f50e10137cdfbe3f413af7444f1f
Jacob <jacobshamer216@gmail.com>
2025-05-15 21:26:26 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,31 @@
+Author: Jacob Shamer
+Course: CMSC 313
+Time Slot: 10:00AM MON/WED
+
+Purpose:
+An assembly program that takes hex data and prints out the characters within, for example: 0x83  ->  83
+
+Files:
+translate2ASCII.s
+
+Build Instructions:
+IDE used: VS Code
+
+translate2ASCII.s:
+
+Compile & Link:
+
+nasm -f elf32 -g -F dwarf -o translate2ASCII.o translate2ASCII.s
+
+ld -m elf_i386 -o translate2ASCII translate2ASCII.o
+
+Additional Information:
+when I run and compile I get the correct output, but I can't tell if the newline is being printed, 
+everything in the buffer is being printed out so I assume it is but I don't really understand why I'm not seeing the newline in the terminal output
+it is in the output buffer as the 24th character and I am printing all 24 characters, all resources I'm looking at are telling me that should work so I don't know
+
+"83 6A 88 DE 9A C3 54 9A\n"
+ ^^^^^^^^^^^^^^^^^^^^^^^^
+ 123456789111111111122222
+          012345678901234
+                     
---
+++ translate2ASCII.s
@@ -0,0 +1,58 @@
+section .data
+    inputBuf db 0x83,0x6A,0x88,0xDE,0x9A,0xC3,0x54,0x9A
+    inputLen dd 8
+    table db "0123456789ABCDEF"                 ; table storing hex characters
+
+section .bss
+    outputBuf resb 80
+
+section .text
+    global _start
+
+_start:
+    xor     ecx, ecx                            ; index, used to iterate through inputBuf
+
+translate:                                      ; loops through the input buffer and translates the data
+    cmp     ecx, [inputLen]                     ; loops until ecx reaches the input buffer length 
+    jge     print                               ; goes to the print section after all the data is translated, (ecx >= 8)
+
+    mov     al, [inputBuf + ecx]                ; move the byte of data from inputBuf at the index into al register
+
+    ; translate the first nibble (4 bits)
+    mov     ah, al                  
+    shr     ah, 4                               ; isolate the first nibble
+    movzx   ebx, ah                             ; stores byte in ebx with the proper bit amount, ebx will be used as an index for the hex table
+    mov     dl, [table + ebx]                   ; store the hex character of the first nibble
+    mov     [outputBuf + ecx*3], dl             ; store the character in the output buffer
+
+    ; translate the second nibble (4 bits)
+    and     al, 0x0F                            ; isolates the second nibble, 0x0F = 00001111 so the first 4 bits of al are set to 0 and the bottom 4 are preserved
+    movzx   ebx, al                             ; stores byte in ebx with the proper bit amount, ebx will be used as an index for the hex table
+    mov     dl, [table + ebx]                   ; store the hex character of the first nibble
+    mov     [outputBuf + ecx*3 + 1], dl         ; store the character in the output buffer
+
+    ; store a space 
+    mov     byte [outputBuf + ecx*3 + 2], ' '   ; needed for formatting the output
+
+    inc ecx                 
+    jmp translate                               ; reinitiate loop
+
+print:
+    mov     byte [outputBuf + ecx*3 - 1], 0x0A  ; replace the final space of the output with a newline character
+    
+    mov     edx, [inputLen]                     ; store the length of the input to print
+    imul    edx, 3                              ; multiply by 3 to get the amount of characters (bytes) total in the output buffer
+
+    ; print output
+    mov     eax, 4                              ; sys_write
+    mov     ebx, 1                              ; stdout      
+    mov     ecx, outputBuf                      ; we want to print the output buffer
+    int     0x80
+
+    ; exit program
+    mov     eax, 1
+    xor     ebx, ebx
+    int     0x80
+
+
+    
